336e98dfffd7e08f8a88b48538e03dbb41b7db55
sampod76 <108483839+sampod76@users.noreply.github.com>
2022-11-08 03:17:22 +0600

    add  middleware and mongodb uri

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
+//meadile wear
+
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.9yhpi6m.mongodb.net/?retryWrites=true&w=majority`;
 
 app.get('/', (req, res) => {
     res.send('this is home service')
