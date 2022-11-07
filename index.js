b8c8d8aae76f4ef006ebe2ce206acaae2e1bc140
sampod76 <108483839+sampod76@users.noreply.github.com>
2022-11-08 03:20:18 +0600

    add dbcollection and test database connection

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
-//meadile wear
+// middleware wear
 
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.9yhpi6m.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const serviceCollaction = client.db('DeliveryService').collection('services')
+const reviewCollaction = client.db('DeliveryService').collection('reviews')
+const test = async (req, res) => {
+    try {
+        await client.connect()
+        console.log('Database connect');
+      
+       
+
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
+test()
 
 app.get('/', (req, res) => {
     res.send('this is home service')
